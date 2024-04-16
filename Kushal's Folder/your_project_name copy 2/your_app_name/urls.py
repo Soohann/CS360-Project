cf8d542624217d91eadc1099457461284696c8a7
Ankeetpaudel <158134679+Ankeetpaudel@users.noreply.github.com>
2024-04-15 22:36:46 -0700

    new

--- Kushal's Folder/your_project_name copy 2/your_app_name/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('student_dashboard.html', views.student_dashboard, name='student_dashboard'),
-]
-
